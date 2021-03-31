31f09d1be23190a61b79f9310bdd9add73dcee69
Tolu <foluab@gmail.com>
2021-03-31 23:22:08 +0100

    first

---
+++ action.js
@@ -0,0 +1,30 @@
+
+
+ function signin(){
+     signinn.classList.toggle("btn-danger");
+    signupp.classList.toggle("btn-danger");
+     form1.style.display="block";
+     form2.style.display="none";
+ }
+
+ function signup(){
+     signinn.classList.toggle("btn-danger");
+     signupp.classList.toggle("btn-danger");
+     form1.style.display="none";
+     form2.style.display="block";;
+ }
+
+// function signin(){
+//     document.getElementById("signupp").classList.remove("btn-danger")
+//     document.getElementById("signinn").classList.add("btn-danger");
+//     document.getElementById("form1").classList.remove("show");
+//     document.getElementById("form2").classList.add("show");
+// }
+
+
+// function signup(){
+//     document.getElementById("signinn").classList.remove("btn-danger");
+//     document.getElementById("signupp").classList.add("btn-danger");
+//     document.getElementById("form2").classList.remove("show");
+//     document.getElementById("form1").classList.add("show");
+// }
